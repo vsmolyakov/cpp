3ce14b012acb2dcdf91459fb677de4bd0cb46170
Kunal Yadav <52342747+kunalyadav684@users.noreply.github.com>
2020-10-01 12:41:42 +0530

    Update dijkstra_algo.cpp

--- greedy/dijkstra_algo.cpp
+++ greedy/dijkstra_algo.cpp
@@ -1,5 +1,3 @@
-// A C++ program for Dijkstra's single source shortest path algorithm. 
-// The program is for adjacency matrix representation of the graph 
 
 #include <limits.h> 
 #include <stdio.h> 
